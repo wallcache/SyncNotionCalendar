0a72ffd061498b4bd8921d1eb88044d3230eb875
Henry Wall <h.wallcache@gmail.com>
2022-02-25 13:08:38 +0000

    file path change

--- syncNotionCalendar.zsh
+++ syncNotionCalendar.zsh
@@ -1,1 +1,1 @@
-/Users/sprietthibault/miniforge3/bin/python /Users/sprietthibault/scripts/notion-ical/main.py
+/Users/henrywall/opt/anaconda3/bin/python /Users/henrywall/Documents/GitHub/SyncNotionCalendar/main.py
